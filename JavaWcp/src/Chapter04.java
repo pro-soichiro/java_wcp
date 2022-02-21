0a068201d69434521d7c6a3a2b7a0cd17c136ced
pro-soichiro <dr.1987626@gmail.com>
2022-02-21 20:56:00 +0900

    4章 確認問題

---
+++ JavaWcp/src/Chapter04.java
@@ -0,0 +1,11 @@
+
+public class Chapter04 {
+	public static void main(String[] args) {
+		String str = "間宮漱一朗";
+		System.out.println(str);
+		str = "人生は快適な空間を出るところから始まる";
+		System.out.println(str);
+		final int NUM = 24;
+		System.out.println(NUM);		
+	}
+}
