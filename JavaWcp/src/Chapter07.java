eb72388cc2d3ca10fa700d5eb5118027c687250d
pro-soichiro <dr.1987626@gmail.com>
2022-02-22 08:21:31 +0900

    7章 確認問題 添削

--- JavaWcp/src/Chapter07.java
+++ JavaWcp/src/Chapter07.java
@@ -4,11 +4,11 @@ public class Chapter07 {
 		int distance = 24;
 		if (distance <= 5) {
 			System.out.println("とても近いです");
-		} else if (distance > 5 && distance <= 10) {
+		} else if (distance <= 10) {
 			System.out.println("近いです");
-		} else if (distance > 10 && distance <= 15) {
+		} else if (distance <= 15) {
 			System.out.println("遠いです");
-		} else if (distance > 15) {
+		} else {
 			System.out.println("とても遠いです");
 		}
 	}
