25ab59772f2357ae71a37b7b05edfcc07ed8689f
pro-soichiro <dr.1987626@gmail.com>
2022-02-21 21:16:40 +0900

    5章 確認問題

---
+++ JavaWcp/src/Chapter05.java
@@ -0,0 +1,21 @@
+
+public class Chapter05 {
+	public static void main(String[] args) {
+		long long1 = 1234567890;
+		double double1 = 0.554d;
+		boolean boolean1 = false;
+		System.out.println(long1);
+		System.out.println(double1);
+		System.out.println(boolean1);
+		
+		Float float1 = 3.141592f;
+		Integer integer1 = 365;
+		Character character1 = 'あ';
+		System.out.println(float1);
+		System.out.println(integer1);
+		System.out.println(character1);
+		
+		int array[] = {1,2,3,4,5};
+		System.out.println(array[3]);
+	}
+}
