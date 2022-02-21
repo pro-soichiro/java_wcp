0dc34d44b9acea1fe9f557557e701b5507734823
pro-soichiro <dr.1987626@gmail.com>
2022-02-21 21:39:07 +0900

    6章 確認問題 解答

---
+++ JavaWcp/src/Chapter06.java
@@ -0,0 +1,17 @@
+
+public class Chapter06 {
+	public static void main(String[] args) {
+		int val = 2 + 5;
+		System.out.println(val);
+		val ++;
+		System.out.println(val);
+		val *= 5;
+		System.out.println(val);
+		boolean bool = val == 50;
+		System.out.println(bool);
+		boolean a = val <= 50;
+		boolean b = val % 10 == 0;
+		bool = a && b;
+		System.out.println(bool);	
+	}
+}
