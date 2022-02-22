74818b927d699f5d8a86e1213a7f4dbc94a3a997
pro-soichiro <dr.1987626@gmail.com>
2022-02-22 09:24:14 +0900

    8章 確認問題 解答

---
+++ JavaWcp/src/Chapter08.java
@@ -0,0 +1,26 @@
+
+public class Chapter08 {
+	public static void main(String[] args) {
+//		1 while
+		int num1 = 1;
+		while (num1 < 5) {
+			int num2 = num1 * num1;
+			System.out.println(num2);
+			num1++;
+		}
+		
+//		2 for
+		int[] array = {1,2,3,4};
+		for (int i = 0; i < array.length; i++) {
+			System.out.println(array[i]);
+		}
+		
+//		3 拡張for
+		for (int number : array ) {
+			if (number % 2 == 0) {
+				continue;
+			}
+			System.out.println(number);
+		}
+	}
+}
