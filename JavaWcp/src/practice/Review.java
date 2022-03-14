373b56edea13db645f98b5cb38834561623ea297
pro-soichiro <dr.1987626@gmail.com>
2022-03-14 10:14:44 +0900

    4章 練習問題

---
+++ JavaWcp/src/practice/Review.java
@@ -0,0 +1,14 @@
+package practice;
+
+public class Review {
+	public static void main(String[] args) {
+		String str = "間宮漱一朗";
+		System.out.println(str);
+		str = "治にいて、乱を忘れず";
+		System.out.println(str);
+		final int NUM = 24;
+		System.out.println(NUM);
+	}
+	
+	
+}
