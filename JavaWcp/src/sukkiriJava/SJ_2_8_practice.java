f98da04e86aa6fed9b9f182d4056101cee2faf9c
pro-soichiro <dr.1987626@gmail.com>
2022-03-14 21:57:22 +0900

    SJ 2章 確認問題の添削

--- JavaWcp/src/sukkiriJava/SJ_2_8_practice.java
+++ JavaWcp/src/sukkiriJava/SJ_2_8_practice.java
@@ -12,6 +12,18 @@ public class SJ_2_8_practice {
 		
 //		2-2
 //		1,5,7
+//		正解は2,4,5,7
+		
+//		2について
+		double d = 2.0F;
+		System.out.println("問題なく" + d + "と出力される");
+		
+//		1について
+//		int num = 3 + 5.0;
+//		System.out.println("問題なく" + num + "と出力されない");
+//		Exception in thread "main" java.lang.Error: Unresolved compilation problem: 
+//			型の不一致: double から int には変換できません
+		
 		
 //		2-3
 		System.out.println("ようこそ占いの館へ");
@@ -23,7 +35,7 @@ public class SJ_2_8_practice {
 		String ageString = new java.util.Scanner(System.in).nextLine();
 		int age = Integer.parseInt(ageString);
 		
-		int fortune = new java.util.Random().nextInt(3);
+		int fortune = new java.util.Random().nextInt(4);
 		fortune ++;
 		
 		System.out.println("占いの結果が出ました！");
