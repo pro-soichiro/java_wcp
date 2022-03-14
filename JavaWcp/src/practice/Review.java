5d6cacb83287127b77f1eedc4084935923026cc7
pro-soichiro <dr.1987626@gmail.com>
2022-03-14 11:03:35 +0900

    7章 確認問題

--- JavaWcp/src/practice/Review.java
+++ JavaWcp/src/practice/Review.java
@@ -47,6 +47,33 @@ public class Review {
 //		よって
 //		bool = (val < 50) && (val % 10 == 0);
 		System.out.println(bool);
+		
+		
+		int distance = 42;
+		if (distance <= 5 ) {
+			System.out.println("とても近いです");
+		} else if (distance <= 10) {
+			System.out.println("近いです");
+		} else if (distance <= 15) {
+			System.out.println("遠いです");
+		} else {
+			System.out.println("とても遠いです");
+		}
+		
+		String signal = "red";
+		switch (signal) {
+		case "red":
+			System.out.println("赤信号です");
+			break;
+		case "yellow":
+			System.out.println("黄信号です");	
+			break;
+		case "blue":
+			System.out.println("青信号です");
+			break;
+		default:
+			System.out.println("信号の色ではありません");
+		}
 	}
 	
 	
