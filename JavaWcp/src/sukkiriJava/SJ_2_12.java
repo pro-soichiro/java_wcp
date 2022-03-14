4d498e8b5eafd8aaecc6a8d38995528473cc804a
pro-soichiro <dr.1987626@gmail.com>
2022-03-14 21:12:16 +0900

    大きい方の数値を代入する命令

---
+++ JavaWcp/src/sukkiriJava/SJ_2_12.java
@@ -0,0 +1,15 @@
+package sukkiriJava;
+
+public class SJ_2_12{
+	
+	public static void main(String[] args) {
+		
+		int a = 5;
+		int b =3;
+		int m = Math.max(a,b);
+		
+		System.out.println(a + "と" + b + "とでは大きい方は" + m + "です");
+		
+	}
+	
+}
