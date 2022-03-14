7661222297ec3e190dde9ce4951547e482ab91e5
pro-soichiro <dr.1987626@gmail.com>
2022-03-14 21:51:39 +0900

    SJ 2章 練習問題の解答

---
+++ JavaWcp/src/sukkiriJava/SJ_2_8_practice.java
@@ -0,0 +1,35 @@
+package sukkiriJava;
+
+public class SJ_2_8_practice {
+	
+	public static void main(String[] args) {
+		
+//		2-1
+		int x = 5;
+		int y = 10;
+		String ans = "x + y は" + ( x + y );
+		System.out.println(ans);
+		
+//		2-2
+//		1,5,7
+		
+//		2-3
+		System.out.println("ようこそ占いの館へ");
+		
+		System.out.println("あなたの名前を入力してください");
+		String name = new java.util.Scanner(System.in).nextLine();
+		
+		System.out.println("あなたの年齢を入力してください");
+		String ageString = new java.util.Scanner(System.in).nextLine();
+		int age = Integer.parseInt(ageString);
+		
+		int fortune = new java.util.Random().nextInt(3);
+		fortune ++;
+		
+		System.out.println("占いの結果が出ました！");
+		System.out.println(age + "歳の" + name + "さん、あなたの運気番号は" + fortune + "です");
+		System.out.println("1:大吉 2:中吉 3:吉 4:凶");		
+		
+	}
+	
+}
