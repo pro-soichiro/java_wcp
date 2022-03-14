65ecbf9b6c65a473986a3ddfb01121365599a64e
pro-soichiro <dr.1987626@gmail.com>
2022-03-14 21:23:26 +0900

    ランダムな数を生成する命令

---
+++ JavaWcp/src/sukkiriJava/SJ_2_14.java
@@ -0,0 +1,13 @@
+package sukkiriJava;
+
+public class SJ_2_14 {
+	
+	public static void main(String[] args) {
+		
+		int r = new java.util.Random().nextInt(90);
+		
+		System.out.println("あなたは多分、" + r + "歳になりますね？");
+		
+	}
+	
+}
