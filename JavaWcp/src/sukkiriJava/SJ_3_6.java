a45abf51948311db4b94dd820edc6ea29bded7de
pro-soichiro <dr.1987626@gmail.com>
2022-03-15 12:12:43 +0900

    あえてbreak文を書かないswitch文の活用

---
+++ JavaWcp/src/sukkiriJava/SJ_3_6.java
@@ -0,0 +1,27 @@
+package sukkiriJava;
+
+public class SJ_3_6 {
+
+	public static void main(String[] args) {
+		// TODO 自動生成されたメソッド・スタブ
+		System.out.println("あなたの運勢を占います");
+		
+//		1~5までの乱数の生成
+		int fortune = new java.util.Random().nextInt(5) + 1;
+		
+		switch (fortune) {
+			case 1:
+			case 2:
+				System.out.println("いいね！");
+				break;
+			case 3:
+				System.out.println("普通です");
+				break;
+			case 4:
+			case 5:
+				System.out.println("うーん...");
+		}
+		
+	}
+
+}
