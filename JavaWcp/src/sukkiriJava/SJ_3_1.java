8dac8a026da1cef17593860a18e6078320e540ce
pro-soichiro <dr.1987626@gmail.com>
2022-03-15 11:51:38 +0900

    天気による行動の変化をJavaで表す

---
+++ JavaWcp/src/sukkiriJava/SJ_3_1.java
@@ -0,0 +1,18 @@
+package sukkiriJava;
+
+public class SJ_3_1 {
+	
+	public static void main(String[] args) {
+		
+		boolean tenki = true;
+		
+		if(tenki) {
+			System.out.println("洗濯をします");
+			System.out.println("散歩に行きます");
+		} else {
+			System.out.println("DVDを見ます");
+		}
+		
+	}
+	
+}
