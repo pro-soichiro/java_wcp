e74d8745aa2b9bb3666298dc66715118a5b453ad
pro-soichiro <dr.1987626@gmail.com>
2022-03-17 12:53:36 +0900

    SJ 4章 確認問題の解答（途中）

---
+++ JavaWcp/src/sukkiriJava/SJ_4_9_practice.java
@@ -0,0 +1,34 @@
+package sukkiriJava;
+
+public class SJ_4_9_practice {
+
+	public static void main(String[] args) {
+		// TODO 自動生成されたメソッド・スタブ
+//		4-1
+		int[] points = new int[4];
+		double[] weights = new double[5];
+		boolean[] answers = new boolean[3];
+		String[] names = new String[3];
+		
+//		4-2
+		int[] moneyList = {121902,8302,55100};
+		
+		for(int i = 0; i < moneyList.length; i++ ) {
+			System.out.println(moneyList[i]);
+		}
+
+		for(int money : moneyList ) {
+			System.out.println(money);
+		}
+		
+//		4-3
+		int[] counts = null;
+		float[] heights = {171.3F, 175.0F};
+		System.out.println(counts[1]);
+//		NullPointerException
+		System.out.println(heights[2]);
+//		ArrayIndexOutOfBounds
+		
+	}
+
+}
