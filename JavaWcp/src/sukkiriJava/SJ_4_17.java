a7ffe6d35c187dce9a9d214a3e5d291a34c8d502
pro-soichiro <dr.1987626@gmail.com>
2022-03-17 12:39:14 +0900

    nulllの利用

---
+++ JavaWcp/src/sukkiriJava/SJ_4_17.java
@@ -0,0 +1,12 @@
+package sukkiriJava;
+
+public class SJ_4_17 {
+
+	public static void main(String[] args) {
+		
+		int[] array = {1,2,3};
+		array = null;
+		array[0] = 10;
+//		NullPointerExceptionになる
+	}
+}
