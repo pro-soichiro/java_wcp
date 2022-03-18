bfde7ead1ba647f19163cedcfca6945f8408027f
pro-soichiro <dr.1987626@gmail.com>
2022-03-18 11:15:57 +0900

    同じ配列を参照していることを確認する

---
+++ JavaWcp/src/sukkiriJava/SJ_5_13.java
@@ -0,0 +1,23 @@
+package sukkiriJava;
+
+public class SJ_5_13 {
+	
+	public static void incArray(int[] array) {
+		
+		for(int i = 0; i < array.length; i++ ) {
+			array[i]++;
+		}
+		
+	}
+	
+	public static void main(String[] args) {
+	
+		int[] array = {1, 2, 3};
+		incArray(array);
+		for(int i : array) {
+			System.out.println(i);
+		}
+		
+	}
+	
+}
