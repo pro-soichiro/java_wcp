a9a59674b3c5efecab3d62abc0d5d368ae63c450
pro-soichiro <dr.1987626@gmail.com>
2022-03-18 09:41:08 +0900

    SJ 4章 確認問題の解答

--- JavaWcp/src/sukkiriJava/SJ_4_9_practice.java
+++ JavaWcp/src/sukkiriJava/SJ_4_9_practice.java
@@ -24,10 +24,21 @@ public class SJ_4_9_practice {
 //		4-3
 		int[] counts = null;
 		float[] heights = {171.3F, 175.0F};
-		System.out.println(counts[1]);
+//		System.out.println(counts[1]);
 //		NullPointerException
-		System.out.println(heights[2]);
-//		ArrayIndexOutOfBounds
+//		System.out.println(heights[2]);
+//		ArrayIndexOutOfBoundsException
+		
+//		4-4
+		int[] numbers = {3,4,9};
+		System.out.println("1桁の数字を入力してください");
+		int input = new java.util.Scanner(System.in).nextInt();
+		for(int num : numbers){
+		    if(num == input){
+		        System.out.println("アタリ！");
+		    }
+		}
+		
 		
 	}
 
