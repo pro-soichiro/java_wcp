f428913210d9484c8a6d24704da0271801f3c65a
pro-soichiro <dr.1987626@gmail.com>
2022-03-19 09:28:12 +0900

    計算機プログラム

---
+++ JavaWcp/src/sukkiriJava/SJ_6_1.java
@@ -0,0 +1,24 @@
+package sukkiriJava;
+
+public class SJ_6_1 {
+
+	public static void main(String[] args) {
+		// TODO 自動生成されたメソッド・スタブ
+		int a = 10;
+		int b = 2;
+		int total = tasu(a, b);
+		int delta = hiku(a, b);
+		
+		System.out.println("足すと" + total + "、引くと" + delta);
+	
+	}
+	
+	public static int tasu(int a, int b) {
+		return ( a + b );
+	}
+	
+	public static int hiku(int a, int b) {
+		return ( a - b );
+	}
+
+}
