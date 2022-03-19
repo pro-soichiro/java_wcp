fcbbbe7af5c621806edcb0e4cb2aa2f1cd6a2965
pro-soichiro <dr.1987626@gmail.com>
2022-03-19 09:37:58 +0900

    計算機プログラムをクラスに分割する

---
+++ JavaWcp/src/sukkiriJava/SJ_6_2.java
@@ -0,0 +1,13 @@
+package sukkiriJava;
+
+public class SJ_6_2 {
+
+	public static int tasu(int a, int b) {
+		return ( a + b );
+	}
+	
+	public static int hiku(int a, int b) {
+		return ( a - b );
+	}
+
+}
---
+++ JavaWcp/src/sukkiriJava/SJ_6_3.java
@@ -0,0 +1,15 @@
+package sukkiriJava;
+
+public class SJ_6_3 {
+
+	public static void main(String[] args) {
+		// TODO 自動生成されたメソッド・スタブ
+		int a = 10;
+		int b = 2;
+		int total = SJ_6_2.tasu(a, b);
+		int delta = SJ_6_2.hiku(a, b);
+		
+		System.out.println("足すと" + total + "、引くと" + delta);
+	}
+
+}
