b8649a395e4a6c8d8f7af968a9f06e724c604a87
pro-soichiro <dr.1987626@gmail.com>
2022-03-20 09:21:20 +0900

    別のパッケージにあるクラスを呼び出す

---
+++ JavaWcp/src/Calc.java
@@ -0,0 +1,16 @@
+package calcapp.main;
+import calcapp.logics.CalcLogic;
+
+public class Calc {
+	
+	public static void main(String[] args) {
+		// TODO 自動生成されたメソッド・スタブ
+		int a = 10;
+		int b = 2;
+		int total = calcapp.logics.CalcLogic.tasu(a, b);
+		int delta = calcapp.logics.CalcLogic.hiku(a, b);
+		
+		System.out.println("足すと" + total + "、引くと" + delta);
+	}
+
+}
