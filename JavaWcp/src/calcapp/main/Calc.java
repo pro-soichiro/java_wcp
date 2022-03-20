307dad88730ebbfe9adc73485997e143475cdd90
pro-soichiro <dr.1987626@gmail.com>
2022-03-20 09:37:09 +0900

    パッケージに対するクラスローダーの動作を理解する

--- JavaWcp/src/calcapp/main/Calc.java
+++ JavaWcp/src/calcapp/main/Calc.java
@@ -7,7 +7,7 @@ public class Calc {
 		// TODO 自動生成されたメソッド・スタブ
 		int a = 10;
 		int b = 2;
-		int total = calcapp.logics.CalcLogic.tasu(a, b);
+		int total = CalcLogic.tasu(a, b);
 		int delta = calcapp.logics.CalcLogic.hiku(a, b);
 		
 		System.out.println("足すと" + total + "、引くと" + delta);
