9710c6236fa5b93a445e2e011d6022f0feeae6cd
pro-soichiro <dr.1987626@gmail.com>
2022-03-21 13:07:33 +0900

    8-7 「眠る」操作に含まれる要素を記述

--- JavaWcp/src/sukkiriJava/Hero.java
+++ JavaWcp/src/sukkiriJava/Hero.java
@@ -7,7 +7,14 @@ public class Hero {
 	int hp;
 	
 	public void attack() {   }
-	public void sleep() {   }
+//	名前：sleep
+//	必要情報：なし
+//	結果：なし
+//	処理内容：眠った後はHPが100に回復する
+	public void sleep() {
+		this.hp = 100;
+		System.out.println(this.name + "は、眠って回復した！");
+	}
 	public void sit(int sec) {   }
 	public void slip() {   }
 	public void run() {   }
