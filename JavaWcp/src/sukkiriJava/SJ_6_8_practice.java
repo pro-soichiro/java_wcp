cb91acfdf3b3061828c52e86a960ecae7f37aeef
pro-soichiro <dr.1987626@gmail.com>
2022-03-21 11:10:53 +0900

    SJ 6章 確認問題の解答 6-1,6-2

--- JavaWcp/src/sukkiriJava/SJ_6_8_practice.java
+++ JavaWcp/src/sukkiriJava/SJ_6_8_practice.java
@@ -1,28 +1,19 @@
 package sukkiriJava;
+import sukkiriJava.comment.Zenhan;
 
 public class SJ_6_8_practice {
 
 	public static void main(String[] args) throws Exception {
 		// TODO 自動生成されたメソッド・スタブ
-		doWarusa();
-		doTogame();
-		callDeae();
-		showMondokoro();
+//		前半
+		Zenhan.doWarusa();
+		Zenhan.doTogame();
+//		後半
+		sukkiriJava.comment.Kouhan.callDeae();
+		sukkiriJava.comment.Kouhan.showMondokoro();
 	}
 	
-	public static  void doWarusa() {
-		System.out.println("きなこでござる。食えませんがの。");
-	}
-	public static  void doTogame() {
-		System.out.println("この老いぼれの目はごまかせませんぞ。");
-	}
-	public static  void callDeae() {
-		System.out.println("えぇい、こしゃくな。くせ者だ！出会えい！");
-	}
-	public static void showMondokoro() throws Exception {
-		System.out.println("飛車さん、角さん。もういいでしょう。");
-		System.out.println("この紋所が目にはいらぬか！");
-		doTogame();
-	}
+	
+	
 
 }
