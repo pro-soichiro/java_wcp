308d2caa5f192bf43cabb9a1a2099a83cb4e178a
pro-soichiro <dr.1987626@gmail.com>
2022-03-21 21:27:12 +0900

    継承クラスでオーバーライド

--- JavaWcp/src/sukkiriJava/SJ_10_4.java
+++ JavaWcp/src/sukkiriJava/SJ_10_4.java
@@ -4,6 +4,9 @@ public class SJ_10_4 {
 
 	public static void main(String[] args) {
 		// TODO 自動生成されたメソッド・スタブ
+		Hero h = new Hero();
+		h.run();
+		
 		SuperHero sh = new SuperHero();
 		sh.run();
 	}
