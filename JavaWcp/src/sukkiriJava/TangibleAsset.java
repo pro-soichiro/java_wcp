c01c4e533b92084745b21ca6cc05d47c0b3bd39b
pro-soichiro <dr.1987626@gmail.com>
2022-03-22 12:40:09 +0900

    SJ_11_6_2 練習問題の解答

--- JavaWcp/src/sukkiriJava/TangibleAsset.java
+++ JavaWcp/src/sukkiriJava/TangibleAsset.java
@@ -2,18 +2,13 @@ package sukkiriJava;
 
 public abstract class TangibleAsset extends Asset {
 	
-	String name;
-	int price;
 	String color;
 	
 	public TangibleAsset(String name,int price,String color) {
-		this.name = name;
-		this.price = price;
+		super(name,price);
 		this.color = color;
 	}
 	
-	public String getName() { return this.name; }
-	public int getPrice() { return this.price; }
 	public String getColor() { return this.color; }
 
 }
